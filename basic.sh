c06383b38e542a53def70072d057572255cbb857
kavithanooji <kavithanooji@gmail.com>
2024-03-14 15:32:50 +0530

    Adding initial scripts

---
+++ basic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Author  : Kavitha
+#Version : 1.0
+Date     : 5-mar-2024
+#Description : The greet
+
+#Define variables
+
+echo -n "Enter your name:"
+read name
+
+echo -n "Enter your salutation :"
+read sal
+
+echo "Hi${sal}.${name}, have a great day"
